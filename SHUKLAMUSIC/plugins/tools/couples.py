046c3a4dd763417ad68f8a9b7a45cb34bda8469c
Black Lover <158266786+BLACKLOVER15@users.noreply.github.com>
2024-03-12 20:21:46 +0530

    Update couples.py

--- SHUKLAMUSIC/plugins/tools/couples.py
+++ SHUKLAMUSIC/plugins/tools/couples.py
@@ -15,7 +15,7 @@ POLICE = [
     [
         InlineKeyboardButton(
             text="𓊈𒆜彡[𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥 𝗪𝗢𝗥𝗟𝗗 ]彡𒆜𓊉",
-            url=f"https://t.me/MASTIWITHFRIENDSX",
+            url=f"https://t.me/Exampur15",
         ),
     ],
 ]
